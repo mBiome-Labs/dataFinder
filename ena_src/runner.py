ed692b4a3abe94e534f73a39be1ee15582deb052
Darren <d.j.wight@gmail.com>
2026-05-06 15:07:25 +0200

    feat: ena scrape scriptadded

---
+++ ena_src/runner.py
@@ -0,0 +1,47 @@
+import sys
+import logging
+import time
+from subprocess import run
+from urllib import parse
+
+RESULT_TYPE = "read_run"
+QUERY = 'tax_tree(3394441) AND (library_source="metagenomic" OR library_source="metatranscriptomic" OR library_source="other")'
+
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.DEBUG,
+    stream=sys.stderr,
+)
+
+
+def main():
+    logging.info("Script Starting...")
+    fields_cmd = (
+        f"curl -X 'GET' 'https://www.ebi.ac.uk/ena/portal/api/returnFields?dataPortal=ena&result={RESULT_TYPE}&format=tsv' "
+        "-H 'accept: */*' | awk '{OFS=\"\t\"; print $1}'"
+    )
+
+    fields = (
+        run(fields_cmd, shell=True, capture_output=True, text=True)
+        .stdout.rstrip()
+        .split("\n")
+    )
+    logging.info(f"Number of fields pulled: {len(fields)}")
+    logging.info(f"Fields pulled: {fields}")
+
+    logging.info(f"Dataset accessed: {RESULT_TYPE}")
+    logging.info(f"Query used: {QUERY}")
+    logging.info("Pulling data from ENA...")
+    tsv_cmd = (
+        "curl -X 'GET' "
+        f"'https://www.ebi.ac.uk/ena/portal/api/search?result={RESULT_TYPE}&query={parse.quote(QUERY)}"
+        f"&fields={parse.quote(','.join(fields[1:]))}&dataPortal=ena&includeMetagenomes=true&format=tsv&download=true' "
+        f"-H 'accept: */*' > ena_rumen_microbiome__{time.strftime('%Y_%m_%d', time.gmtime())}.tsv"
+    )
+    run(tsv_cmd, shell=True)
+    logging.info("Script Finished!")
+
+
+if __name__ == "__main__":
+    main()
